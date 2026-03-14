f61eb2f2bebc57b1c92bfea7da6dbfe8d777b7b6
Ibrahim Fatade <ibfatade18@gmail.com>
2026-03-14 15:35:52 +0000

    feat: Add property models

---
+++ Server/models/properties.js
@@ -0,0 +1,42 @@
+const db = require('../helpers/database'); 
+
+exports.getAll = async function getAll() {
+    const query = "SELECT * FROM properties;";
+    const data = await db.run_query(query);
+    return data;
+}
+
+exports.getById = async function getById (id) {
+    const query = "SELECT * FROM properties WHERE ID = ?;";
+    const data = await db.run_query(query, [id]);
+    return data;
+}
+
+exports.add = async function add (property) {
+    const query = "INSERT INTO properties SET ?;";
+    const data = await db.run_query(query, [property]);
+    return data;
+}
+
+// exports.deleteById = async function deleteById (id) {
+//     const query = "DELETE FROM properties WHERE ID = ?;";
+//     const data = await db.run_query(query, [id]);
+//     return data;
+// }
+
+// exports.updateById = async function updateById (id, fieldsToUpdate) {
+//     const setClause = Object.keys(fieldsToUpdate).map(key => `${key} = ?`).join(', ');
+//     const values = Object.values(fieldsToUpdate);
+//     values.push(id);
+//     const query = `UPDATE properties SET ${setClause} WHERE ID = ?;`;
+//     const data = await db.run_query(query, values);
+//     return data;
+// }
+
+
+// exports.search = async function search (searchTerm, page, limit) {
+//     const offset = (page - 1) * limit;
+//     const query = "SELECT * FROM properties WHERE title LIKE ? LIMIT ? OFFSET ?;";
+//     const data = await db.run_query(query, [`%${searchTerm}%`, limit, offset]);
+//     return data;
+// }
