88a136f3cef3e50f8c133c33e938437c7070ae9c
Ibrahim Fatade <ibfatade18@gmail.com>
2026-03-14 07:53:39 +0000

    Enable users API routes

--- Server/app.js
+++ Server/app.js
@@ -2,9 +2,10 @@ const Koa = require('koa');
 const cors = require('@koa/cors');
 const app = new Koa();
 const properties = require('./routes/properties.js');
-// const users = require('./routes/users.js');
+const users = require('./routes/users.js');
 
 app.use(cors());
 app.use(properties.routes());
+app.use(users.routes());
 app.listen(3000);
 console.log('Server running on port 3000');
