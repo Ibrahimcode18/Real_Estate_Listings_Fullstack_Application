ab1ac79bb3711f7cb81772c6aa7b2f76d3e6c6b6
Ibrahim Fatade <ibfatade18@gmail.com>
2026-03-15 16:28:53 +0000

    feat: Add agents models

---
+++ Server/models/agents.js
@@ -0,0 +1,28 @@
+const db = require('../helpers/database');
+
+exports.getById = async function getById (id) {
+    const query = "SELECT * FROM agents WHERE ID = ?;";
+    const data = await db.run_query(query, [id]);
+    return data;
+}
+
+exports.getAll = async function getAll () {
+    const query = "SELECT * FROM agents;";
+    const data = await db.run_query(query);
+    return data;
+}
+
+exports.add = async function add (agent) {
+    const query = "INSERT INTO agents (user_id, agency_name, phone_number, license_number, about, is_approved) VALUES (?, ?, ?, ?, ?, ?);";
+    const data = await db.run_query(query, [agent.user_id, agent.agency_name, agent.phone_number, agent.license_number, agent.about, agent.is_approved]);
+    return data;
+}
+
+exports.updateById = async function updateById (id, fieldsToUpdate) {
+    const setClause = Object.keys(fieldsToUpdate).map(key => `${key} = ?`).join(', ');
+    const values = Object.values(fieldsToUpdate);
+    values.push(id);
+    const query = `UPDATE agents SET ${setClause} WHERE ID = ?;`;
+    const data = await db.run_query(query, values);
+    return data;
+}
