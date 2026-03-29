5c983a39ee88ebd0e61c3388bdbad057a130f53d
Ibrahim Fatade <ibfatade18@gmail.com>
2026-03-29 23:17:17 +0100

    feat: Separate nav items based on roles

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,85 +1,75 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+  import { RouterView, RouterLink } from 'vue-router'
+  import { useUserStore } from '@/stores/user' // Import Store
+
+  const userStore = useUserStore()
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <a-layout class="layout">
+    <a-layout-header>
+      <div class="logo">CR7 Estate Properties</div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <a-menu theme="dark" mode="horizontal" :selectable="false">
+        <a-menu-item key="1"><RouterLink to="/">Home</RouterLink></a-menu-item>
+        <a-menu-item key="6"><RouterLink to="/locations">Locations</RouterLink></a-menu-item>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+        <template v-if="userStore.user.loggedIn">
+          <template v-if="userStore.user.role === 'admin'">
+            <a-menu-item key="4"><RouterLink to="/dashboard">Dashboard</RouterLink></a-menu-item>
+          </template>
+          <template v-if="userStore.user.agent_id !== null">
+            <a-menu-item key="8"><RouterLink to="/agentproperties">My Properties</RouterLink></a-menu-item>
+          </template>
+        </template>
 
-  <RouterView />
+        <template v-if="!userStore.user.loggedIn">
+            <a-menu-item key="2" style="margin-left: auto">
+                <RouterLink to="/login">Login</RouterLink>
+            </a-menu-item>
+            <a-menu-item key="3"><RouterLink to="/register">Register</RouterLink></a-menu-item>
+        </template>
+
+        <template v-else>
+            <a-menu-item key="5" style="margin-left: auto">
+                Hello, {{ userStore.user.username }}
+            </a-menu-item>
+            <a-menu-item key="7"><RouterLink to="/profile">Profile</RouterLink></a-menu-item>
+            <a-menu-item key="9"><RouterLink to="/login" @click="userStore.logout">Logout</RouterLink></a-menu-item>
+        </template>
+    </a-menu>
+    </a-layout-header>
+
+    <a-layout-content style="padding: 0 50px; margin-top: 20px">
+      <div class="site-layout-content">
+        <RouterView />
+      </div>
+    </a-layout-content>
+    <a-layout-footer style="text-align: center">
+      Web API Development ©2026 Created by Me
+    </a-layout-footer>
+  </a-layout>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .site-layout-content {
+    background: #fff;
+    padding: 24px;
+    min-height: 280px;
+    margin-top: 24px;
   }
-
   .logo {
-    margin: 0 2rem 0 0;
+    float: left;
+    width: 300px;
+    height: 31px;
+    color: white;
+    font-size: 1.5rem;
+    font-weight: bold;
+    line-height: 64px; 
   }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+  .router-link-active {
+    color: #ffffff !important;
+    font-weight: bold;
   }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
