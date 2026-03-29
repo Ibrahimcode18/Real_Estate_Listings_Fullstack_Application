e9537c6bc3579a62f1a223428239a6a263c0c7a2
Ibrahim Fatade <ibfatade18@gmail.com>
2026-03-29 23:12:36 +0100

    feat: Added component to display Locations

---
+++ Client/src/components/LocationCard.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { Card } from 'ant-design-vue';
+import { Tag } from 'ant-design-vue';
+import { LikeOutlined, MessageOutlined, LikeFilled } from '@ant-design/icons-vue';
+import { ref } from 'vue'
+
+    const liked = ref(false);
+// Define what data this component EXPECTS to receive
+    defineProps({
+        name: String,
+        imageURL: String 
+    })
+
+</script>
+<template>
+    <a-card hoverable style="width: 300px; overflow: hidden;">
+        <template #cover>
+            <div style="position: relative; height: 200px;">
+                <img :alt="name" :src="imageURL || 'https://picsum.photos/300/200'" 
+                    style="width: 100%; height: 100%; object-fit: cover;" 
+                />
+                <div class="location-name">
+                    {{ name }}
+                </div>
+            </div>
+        </template>
+    </a-card>
+</template>
+
+<style scoped>
+    .location-name{
+        position: absolute; 
+        bottom: 0px; 
+        text-align: center;
+        width: 100%; 
+        padding: 10px 15px; 
+        color: white; 
+        font-weight: bold; 
+        font-size: 1.5rem;
+        background: linear-gradient(to top, rgba(0,0,0,0.7), transparent);
+    }
+</style>
