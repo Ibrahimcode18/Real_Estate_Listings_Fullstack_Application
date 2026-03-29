d51bb57c36ddc0ebbe0fe2efb72496b1af862990
Ibrahim Fatade <ibfatade18@gmail.com>
2026-03-29 23:07:21 +0100

    feat: Added update and delete location buttons

--- Client/src/views/LocationsView.vue
+++ Client/src/views/LocationsView.vue
@@ -1,9 +1,15 @@
 <script setup>
-    import { ref, onMounted } from 'vue'  
-    import LocationCard from '@/components/LocationCard.vue'
+    import { ref, onMounted } from 'vue';
+    import { useUserStore } from '@/stores/user';  
+    import LocationCard from '@/components/LocationCard.vue';
+    import LocationModal from '@/components/LocationModal.vue';
 
     const locations = ref([])
     const loading = ref(true)
+    const userStore = useUserStore()
+    
+    const isModalVisible = ref(false)
+    const selectedLocation = ref(null)
 
     onMounted(async () => {
         try {
@@ -17,19 +23,79 @@
             loading.value = false
         }
     })
+
+    const openAddModal = () => {
+        selectedLocation.value = null
+        isModalVisible.value = true
+    }
+
+    const openUpdateModal = (location) => {
+        selectedLocation.value = location
+        isModalVisible.value = true
+    }
+
+    const handleLocationSaved = (savedLocation, wasEditing) => {
+        if (wasEditing) {
+            const index = locations.value.findIndex(loc => loc.id === savedLocation.id)
+            if (index !== -1) locations.value[index] = savedLocation
+        } else {
+            locations.value.push(savedLocation)
+        }
+    }
+
+    const handleDeleteLocation = async (id) => {
+        if (confirm('Are you sure you want to delete this location?')) {
+            try {
+                const res = await fetch(`http://localhost:3000/api/v1/locations/${id}`, {
+                    method: 'DELETE',
+                    headers: { 'Authorization': `Bearer ${localStorage.getItem('token')}` }
+                })
+                if (res.ok) {
+                    locations.value = locations.value.filter(loc => loc.id !== id)
+                }
+            } catch (e) {
+                console.error('Failed to delete:', e)
+            }
+        }
+    }
 </script>
 <template>
-  <h1>Locations</h1>
-  <div v-if="loading">Loading...</div>
+    <div style="display: flex; justify-content: space-between; 
+        align-items: center; margin-bottom: 20px;">
+        <h1 style="margin: 0;">Locations</h1>
+        
+        <a-button type="primary" v-if="userStore.user?.role === 'admin'" 
+            @click="openAddModal" size="large"style="font-weight: bold;"
+        >
+            Add Location
+        </a-button>
+    </div>
+    <div v-if="loading">Loading...</div>
 
-  <a-row :gutter="[16, 16]" v-else>
-    <a-col :xs="24" :sm="12" :md="12" :lg="8" :xl="6" v-for="location in locations" :key="location.id">
-        <router-link :to="`locations/${location.id}?name=${location.city}`">
-          <LocationCard
-              :name="location.city"
-              :imageURL="location.imageURL"
-          />
-      </router-link>
-    </a-col>
-  </a-row>
+    <a-row :gutter="[16, 16]" v-else>
+        <a-col :xs="24" :sm="12" :md="12" :lg="8" :xl="6" v-for="location in locations" :key="location.id">
+            <router-link :to="`locations/${location.id}?name=${location.city}`">
+                <LocationCard
+                    :name="location.city"
+                    :imageURL="location.imageURL"
+                />
+            </router-link>
+            <div 
+                v-if="userStore.user?.role === 'admin'" 
+                style="margin-top: 10px; display: flex; gap: 10px; justify-content: center;"
+            >
+                <a-button size="small" type="primary" ghost @click="openUpdateModal(location)">
+                    Update
+                </a-button>
+                <a-button size="small" type="primary" danger @click="handleDeleteLocation(location.id)">
+                    Delete
+                </a-button>
+            </div>
+        </a-col>
+    </a-row>
+    <LocationModal 
+      v-model:open="isModalVisible" 
+      :initialData="selectedLocation" 
+      @saved="handleLocationSaved"
+    />
 </template>
