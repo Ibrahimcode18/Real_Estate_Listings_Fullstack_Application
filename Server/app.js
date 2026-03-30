00bf30df835001c11de2abc2f1edb3ee93cb2dfc
Ibrahim Fatade <ibfatade18@gmail.com>
2026-03-30 16:45:46 +0100

    chore: Installes and setup OAS documentation tools

--- Server/app.js
+++ Server/app.js
@@ -3,20 +3,26 @@ const cors = require('@koa/cors');
 const app = new Koa();
 const { koaBody } = require('koa-body');
 
+const { koaSwagger } = require('koa2-swagger-ui');
+const yaml = require('yamljs');
+
+const port = 3000;
 const properties = require('./routes/properties.js');
 const users = require('./routes/users.js');
 const locations = require('./routes/locations.js');
 const agents = require('./routes/agents.js');
 const uploads = require('./routes/uploads');
+const spec = yaml.load('./schemas/openapi.yaml');
 
 app.use(koaBody({
-    multipart: true, // This enables file catching
+    multipart: true,
     formidable: {
         keepExtensions: true,
         maxFileSize: 5 * 1024 * 1024,
     },
     parsedMethods: ['POST', 'PUT', 'PATCH', 'DELETE']
 }));
+app.use(koaSwagger({ routePrefix: '/api/v1/docs', swaggerOptions: { spec } }));
 
 app.use(cors());
 app.use(properties.routes());
@@ -25,5 +31,10 @@ app.use(locations.routes());
 app.use(agents.routes());
 app.use(uploads.routes());
 
-app.listen(3000);
-console.log('Server running on port 3000');
+
+if (process.env.NODE_ENV !== 'test') { 
+    app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+}
+module.exports = app; 
--- Server/config.js
+++ Server/config.js
@@ -1,7 +1,18 @@
-exports.config = {
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Ibrahimcode18",
-    database: "realestate_listings"
+if (process.env.NODE_ENV === 'test') {
+    console.log(`TEST MODE DETECTED: Connecting to real_estate_test_db`);
+    exports.config = {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Ibrahimcode18",
+        database: "real_estate_test_db"
+    }
+} else {
+    exports.config = {
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Ibrahimcode18",
+        database: "realestate_listings"
+    }
 }
