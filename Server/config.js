7b0c140dc9cbff3cf3ca9a59cb59566967368794
Ibrahim Fatade <ibfatade18@gmail.com>
2026-06-30 20:44:23 +0100

    fix: Added host parameter from .env

--- Server/config.js
+++ Server/config.js
@@ -10,8 +10,8 @@ if (process.env.NODE_ENV === 'test') {
     }
 } else {
     exports.config = {
-        host: "localhost",
-        port: 3306,
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || 3306, 
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME
