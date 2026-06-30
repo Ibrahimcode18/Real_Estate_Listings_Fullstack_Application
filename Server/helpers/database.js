98a780537d6a4207d60eb2514896573e60d49219
Ibrahim Fatade <ibfatade18@gmail.com>
2026-06-30 12:09:08 +0100

    Modified database connection for production compatibility

--- Server/helpers/database.js
+++ Server/helpers/database.js
@@ -4,7 +4,12 @@ const { config } = require('../config');
 
 exports.run_query = async function run_query(query, values) {
     try {
-        const connection = await mysql.createConnection(config);
+        const isProduction = process.env.NODE_ENV === 'production';
+
+        const connection = await mysql.createConnection({
+            ...config,
+            ssl: isProduction ? { rejectUnauthorized: false } : undefined
+        });
         const [data] = await connection.query(query, values);
         await connection.end();
         return data;
