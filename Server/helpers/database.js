a6c8a659aaeb8e2b99ef17d299d226d26fe3a78d
Ibrahim Fatade <ibfatade18@gmail.com>
2026-06-30 21:21:33 +0100

    Replaced single connections with connection pool

--- Server/helpers/database.js
+++ Server/helpers/database.js
@@ -1,17 +1,20 @@
 const mysql = require('mysql2/promise');
 const { config } = require('../config');
 
+const isProduction = process.env.NODE_ENV === 'production';
+const poolConfig = {
+    ...config,
+    ssl: isProduction ? { rejectUnauthorized: false } : undefined,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+};
+
+const pool = mysql.createPool(poolConfig);
 
 exports.run_query = async function run_query(query, values) {
     try {
-        const isProduction = process.env.NODE_ENV === 'production';
-
-        const connection = await mysql.createConnection({
-            ...config,
-            ssl: isProduction ? { rejectUnauthorized: false } : undefined
-        });
-        const [data] = await connection.query(query, values);
-        await connection.end();
+        const [data] = await pool.query(query, values);
         return data;
     } catch (error) {
         console.error("Database Error:", error);
